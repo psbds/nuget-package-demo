b5d1fbc9a8dff94d784fff30beccb097392d2e39
psbds <padasil@microsoft.com>
2020-08-25 17:03:59 -0300

    init repo

---
+++ DemoNuget.Library/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoNuget.Library
+{
+	public class Calculator
+	{
+		public double Sum(double number1, double number2)
+		{
+			return number1 + number2;
+		}
+	}
+}
---
+++ DemoNuget.Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using DemoNuget.Library;
+using System;
+
+namespace DemoNuget.Test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var calculator = new Calculator();
+			Console.WriteLine(calculator.Sum(1, 1));
+		}
+	}
+}
